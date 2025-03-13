c64b6ec88eeec2ff6d4dbadd7c78bc18c0fcf29e
Haytra-Davia <asaduasyuja@gmail.com>
2025-03-13 22:40:24 +0700

    baru

---
+++ Home/home.js
@@ -0,0 +1,5 @@
+
+        document.getElementById("toggleBtn").addEventListener("click", function(event) {
+            event.preventDefault();
+            document.getElementById("sidebar").classList.toggle("shrink");
+        });
---
+++ html/satu.js
@@ -0,0 +1,18 @@
+const darkModeToggle = document.querySelector('.toggle-switch');
+    const body = document.body;
+
+    // Check localStorage for theme preference
+    if(localStorage.getItem('theme') === 'dark') {
+        body.classList.add('dark-theme');
+    }
+
+    darkModeToggle.addEventListener('click', () => {
+        body.classList.toggle('dark-theme');
+        
+        // Save preference to localStorage
+        if(body.classList.contains('dark-theme')) {
+            localStorage.setItem('theme', 'dark');
+        } else {
+            localStorage.setItem('theme', 'light');
+        }
+    });
