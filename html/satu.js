b2854de9bf9a2d4fa10dbb67b3ecccaff2441500
Haytra-Davia <asaduasyuja@gmail.com>
2025-03-16 23:29:30 +0700

    selesai

--- html/satu.js
+++ html/satu.js
@@ -1,22 +1,22 @@
 const darkModeToggle = document.querySelector('.toggle-switch');
-    const body = document.body;
+const body = document.body;
 
-    // Check localStorage for theme preference
-    if(localStorage.getItem('theme') === 'dark') {
-        body.classList.add('dark-theme');
+// Check localStorage for theme preference
+if (localStorage.getItem('theme') === 'dark') {
+    body.classList.add('dark-theme');
+}
+
+darkModeToggle.addEventListener('click', () => {
+    body.classList.toggle('dark-theme');
+
+    // Save preference to localStorage
+    if (body.classList.contains('dark-theme')) {
+        localStorage.setItem('theme', 'dark');
+    } else {
+        localStorage.setItem('theme', 'light');
     }
+});
 
-    darkModeToggle.addEventListener('click', () => {
-        body.classList.toggle('dark-theme');
-        
-        // Save preference to localStorage
-        if(body.classList.contains('dark-theme')) {
-            localStorage.setItem('theme', 'dark');
-        } else {
-            localStorage.setItem('theme', 'light');
-        }
-    });
-
-    function toggleMenu() {
-        document.querySelector(".nav-links").classList.toggle("active");
-    }
+function toggleMenu() {
+    document.querySelector(".nav-links").classList.toggle("active");
+}
