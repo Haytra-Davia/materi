5425900a27c15dd1b05f194a66dd339c40c577af
Haytra-Davia <asaduasyuja@gmail.com>
2025-03-24 11:25:44 +0700

    tipe data

--- javascripth/bagian-pengenalan.js
+++ javascripth/bagian-pengenalan.js
@@ -1,4 +1,27 @@
-if("true"){
-    var nama = ("nama saya haytra");
-}
-document.write(nama);
+// if("true"){
+//     var nama = ("nama saya haytra");
+// }
+// document.write(nama);
+
+// type data
+
+// let orang ={
+//     nama: "haytra",
+//     kelas: 19,
+//     tinggal: "di bumi"
+// };
+// console.log(orang, typeof orang)
+
+// let arayy = ['hallo', 12344, true, 12.33];
+// console.log(arayy, typeof arayy)
+
+// function list(){
+//     return "namamu siapa?";
+// }
+// console.log(list, typeof list)
+
+// let besok = new Date();
+// console.log(besok, typeof besok)
+
+let regex = /hallo/o;
+console.log(regex, typeof regex)
