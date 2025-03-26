655e504281cc30a5c3a5d4401bb03c4e5eb88341
Haytra-Davia <asaduasyuja@gmail.com>
2025-03-26 14:12:56 +0700

    soal

--- javascripth/bagian-pengenalan.js
+++ javascripth/bagian-pengenalan.js
@@ -23,5 +23,66 @@
 // let besok = new Date();
 // console.log(besok, typeof besok)
 
-let regex = /hallo/o;
-console.log(regex, typeof regex)
+// let regex = /hallo/o;
+// console.log(regex, typeof regex)
+
+
+// operator logika
+// let a = true
+// let s = false
+// let result = !a; 
+// document.write(result)
+
+// array
+// let jaman = 'hallo';
+// let km = jaman;
+// document.write(km[2])
+// console.log(km[2])
+
+//mengetahui panjang str
+// let nama ='hadi'
+// document.write(nama.length)
+
+// mengubah hurup besar 
+// let nama ='hadi'
+// document.write(nama.toUpperCase())
+
+// mengubah kecil
+// let nama ='hadi'
+// document.write(nama.toLowerCase())
+
+//menghilangkan spasi
+// let nama ='hadi hadi         '
+// document.write(nama.trim())
+
+//menambahkan variable
+// let nama = "hadi"
+// let depan = "bayu"
+// document.write(`${nama} ${depan}`, " ","saputra")
+
+//mengambil bagian dari string
+// let nama = "hadi bayu"
+// document.write(nama.slice(0,3))
+// document.write(nama.substring(0,2))
+
+//mengganti bagian dari string 
+// let ambil = "hallo , hadi";
+// let habis = ambil.replace('hallo' , 'jangan')
+// document.write(habis)
+
+//gabungkan string dengan arayy
+// let aray = ['hadi', 'bayu' , 'saputra']
+// document.write(aray.join(" "))
+// document.write('<br>')
+// document.write(aray.splice('-' ,2))
+
+//mencari kata 
+// let cari = "hadi haytra hadi haytra hadi haytra hadi haytra"
+// // dia mencari yang lebih awal
+// document.write(cari.indexOf("haytra"))
+// document.write('<br>')
+// // mencari lebih ahir
+// document.write(cari.lastIndexOf("haytra"))
+// document.write('<br>')
+// // mencari keberadaanya
+// document.write(cari.includes('saputra'))
---
+++ javascripth/tugas/tugas1.js
@@ -0,0 +1,19 @@
+// variable
+// let nama = "haytra";
+// let umur ="39";
+// let tinggi = 1.5;
+
+// let semua = (`"nama = " ${nama} '<br>' "umur = "${umur} '<br>' "tinggi = "${tinggi}`);
+// document.write(semua);
+// console.log(semua)
+
+//type concersion
+let string  = "456"
+let number = 77.7
+let bol = false
+
+let str = parseInt(string);
+let num = number.toString();
+let numb = parseInt(bol);
+
+document.write(`${str} ${num} ${numb}`)
