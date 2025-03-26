e0aa82a70850b95b857047f607021f17f6e85b86
Haytra-Davia <asaduasyuja@gmail.com>
2025-03-26 20:58:45 +0700

    soal

--- javascripth/tugas/tugas1.js
+++ javascripth/tugas/tugas1.js
@@ -8,12 +8,57 @@
 // console.log(semua)
 
 //type concersion
-let string  = "456"
-let number = 77.7
-let bol = false
+// let string  = "456"
+// let number = 77.7
+// let bol = false
 
-let str = parseInt(string);
-let num = number.toString();
-let numb = parseInt(bol);
+// let str = parseInt(string);
+// let num = number.toString();
+// let numb = parseInt(bol);
 
-document.write(`${str} ${num} ${numb}`)
+// document.write(`${str} ${num} ${numb}`)
+
+// Minta input dari pengguna
+// let pr1 = parseInt(prompt("masukan angka pertama"))
+// let pr2 = parseInt(prompt("masukan angka 2"))
+
+// if(!isNaN(pr1) && !isNaN(pr2)){
+//     document.write(`pejumlahan = ${pr1} + ${pr2} = ${pr1 + pr2}`)
+//     document.write("<br>")
+//     document.write(`pengurangan = ${pr1} - ${pr2} = ${pr1 - pr2}`)
+//     document.write("<br>")
+//     document.write(`perkalian = ${pr1} x ${pr2} = ${pr1 * pr2}`)
+//     document.write("<br>")
+//     document.write(`pembagian = ${pr1} : ${pr2} = ${pr1 / pr2}`)
+// } 
+// else{
+//     document.write("masukan angka")
+// }
+
+//operator ternary
+// let sim = parseInt(prompt("masukan umur"))
+// let sim1 = sim
+// if(sim1 >= 17){
+//     document.write("pas umur lu")
+// }else{
+//     document.write("kurang pas")
+// }
+
+//challenge operator
+// let challenge = parseFloat(prompt("masukan angka"))
+//     if(challenge % 2 === 0){
+//         document.write("genap")
+//     }else{
+//         document.write("ganjil")
+//     }
+
+//string
+// let str = prompt("masukan tulisan")
+// if(isNaN(str)){
+// document.write(str.toUpperCase())
+// }else{
+//     document.write("iya")
+// }
+
+// Ambil angka dari user
+let h = parseInt(prompt())
